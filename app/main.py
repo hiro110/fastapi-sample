c020821fa3372cf37361bcd3f5a646e2709258bb
hiro110 <h.ito198701@gmail.com>
2021-02-07 17:03:55 +0900

    update

--- app/main.py
+++ app/main.py
@@ -1,13 +1,11 @@
 import sys, os
 from enum import Enum
+from typing import List, Optional
 
 from fastapi import FastAPI
-from typing import List, Optional
 from starlette.middleware.cors import CORSMiddleware
 
-# from db import Session
 from routers import users, items
-from middlewares import sample
 
 app = FastAPI()
 
@@ -18,7 +16,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-# app.add_middleware(sample.SampleMiddleware)
 
 @app.get("/")
 async def root():
